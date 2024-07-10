077f2e1e42ef332906c9d1e31522e5f6f63e73a7
barangievans <barangievans@gmail.com>
2024-07-10 18:21:52 +0300

    control-flow

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let message;
+  if (distance <=400){
+    message = 'This one is on me!';
+  }else if (distance > 400 && distance <= 2000) {
+    message = 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case 'generous':
+          return 'Thank you so much.';
+      case 'not as generous':
+          return 'Thank you.';
+      default:
+          return 'Bye.';
+  }
 }
